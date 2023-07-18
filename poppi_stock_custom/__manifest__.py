f9e0f294117c44c713318bafa6001d01b4f509f8
kaynnan <kaynnan.lemes@escodoo.com.br>
2023-07-18 19:34:13 -0300

    [IMP] poppi_stock_custom: report minuta
    [IMP] poppi_stock_custom: report rotulo

--- poppi_stock_custom/__manifest__.py
+++ poppi_stock_custom/__manifest__.py
@@ -2,17 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Poppi Stock Custom',
-    'summary': """
+    "name": "Poppi Stock Custom",
+    "summary": """
         Customizações no módulo Stock""",
-    'version': '14.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Escodoo',
-    'website': 'https://github.com/Escodoo/poppi-addons',
-    'depends': [
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/poppi-addons",
+    "depends": ["l10n_br_stock", "l10n_br_delivery_nfe"],
+    "data": [
+        "report/report_poppi.xml",
+        "report/report_poppi_minuta.xml",
     ],
-    'data': [
-    ],
-    'demo': [
-    ],
+    "demo": [],
 }
