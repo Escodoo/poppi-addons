b8b3b430bdcebfc497a074fc4abf9a016413e1f8
kaynnan <kaynnan.lemes@escodoo.com.br>
2023-07-18 11:03:53 -0300

    [ADD] poppi_stock_custom: create addons

---
+++ poppi_stock_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Poppi Stock Custom',
+    'summary': """
+        Customizações no módulo Stock""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    'website': 'https://github.com/Escodoo/poppi-addons',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
