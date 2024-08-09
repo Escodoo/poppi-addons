4423c18eb0c0d8cc1d06ba37d29bcb844b958087
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-08-09 15:57:12 -0300

    [ADD] l10n_br_fiscal_difal_simple_for_all_uf:  new module

---
+++ l10n_br_fiscal_difal_simple_for_all_uf/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Brazillian Fiscal DIFAL Simple in all UF",
+    "version": "14.0.1.0.0",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/poppi-addons",
+    "depends": ["l10n_br_fiscal"],
+}
