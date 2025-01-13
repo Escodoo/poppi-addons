f65e44dab411f5a24c60e87e45476a6afb3d9423
kaynnan <kaynnanx1@gmail.com>
2025-01-12 23:55:14 -0300

    [ADD] poppi_account_custom: add new module

---
+++ poppi_account_custom/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2025 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Poppi Account Custom",
+    "summary": """
+        Poppi Account Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/poppi-addons",
+    "depends": ["account"],
+    "data": ["views/report_invoice.xml"],
+}
